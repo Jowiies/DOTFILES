b0c91867fa03527f5ceff1320a8e6f44e6a85fc8
Joel Esteller <joel@thinkpad>
2025-07-11 16:31:33 +0200

    Thinkpad Debian Rice

---
+++ sway/scripts/lock.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+swaylock \
+  --screenshots \
+  --clock \
+  --text-color ffffff \
+  --indicator \
+  --indicator-radius 100 \
+  --indicator-thickness 7 \
+  --effect-blur 7x5 \
+  --effect-vignette 0.5:0.5 \
+  --ring-color e60012 \
+  --key-hl-color e60012 \
+  --line-color 00000000 \
+  --inside-color 00000088 \
+  --separator-color 00000000 \
+  --grace 2 \
+  --fade-in 0.2 \
+  --daemonize
